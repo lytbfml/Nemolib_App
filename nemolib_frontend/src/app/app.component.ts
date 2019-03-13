38d48e30e9ff8bb10589bed8d610df2f3754505c
Yangxiao Wang <lytbfml@hotmail.com>
2019-03-13 03:27:46 -0700

    change UI

--- nemolib_frontend/src/app/app.component.ts
+++ nemolib_frontend/src/app/app.component.ts
@@ -1,13 +1,56 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {FormControl, FormGroupDirective, NgForm, Validators} from '@angular/forms';
+import {ErrorStateMatcher} from '@angular/material';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent {
-  title = 'Nemolib-ng-app';
-  public size = 3;
+/** Error when invalid control is dirty, touched, or submitted. */
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+    isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+        const isSubmitted = form && form.submitted;
+        return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+    }
 }
 
+@Component({
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+    title = 'Nemolib-ng-app';
+    public motifSize = 3;
+    public randSize = 10;
+    public probSel: number;
+    public prob = [0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5];
+    private valid: boolean;
+    public results = 'Results';
+    mSControl = new FormControl('', [
+        Validators.required,
+        Validators.min(3),
+        Validators.max(8)
+    ]);
+    rSControl = new FormControl('', [
+        Validators.required,
+        Validators.min(10)
+    ]);
+
+    matcher = new MyErrorStateMatcher();
+    file: any;
+
+    postData(): void {
+        this.valid = true;
+        for (let i = 0; i < this.motifSize; i++) {
+            if (this.prob[i] == null || this.prob[i] <= 0 || this.prob[i] > 1) {
+                this.valid = false;
+            }
+        }
+        if (this.probSel == null || this.motifSize == null || this.randSize == null) {
+            this.valid = false;
+        }
+        if (this.valid) {
+            this.results = this.motifSize + ' ,  ' + this.prob;
+        } else{
+            this.results = 'not valid';
+        }
+    }
+}
 
--- nemolib_frontend/src/app/app.module.ts
+++ nemolib_frontend/src/app/app.module.ts
@@ -1,10 +1,56 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-import {FormsModule} from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {HttpClientModule} from '@angular/common/http';
+
+import {A11yModule} from '@angular/cdk/a11y';
+import {DragDropModule} from '@angular/cdk/drag-drop';
+import {PortalModule} from '@angular/cdk/portal';
+import {ScrollingModule} from '@angular/cdk/scrolling';
+import {CdkStepperModule} from '@angular/cdk/stepper';
+import {CdkTableModule} from '@angular/cdk/table';
+import {CdkTreeModule} from '@angular/cdk/tree';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {
+    MatAutocompleteModule,
+    MatBadgeModule,
+    MatBottomSheetModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatChipsModule,
+    MatDatepickerModule,
+    MatDialogModule,
+    MatDividerModule,
+    MatExpansionModule,
+    MatGridListModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatMenuModule,
+    MatNativeDateModule,
+    MatPaginatorModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    MatRadioModule,
+    MatRippleModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatSliderModule,
+    MatSlideToggleModule,
+    MatSnackBarModule,
+    MatSortModule,
+    MatStepperModule,
+    MatTableModule,
+    MatTabsModule,
+    MatToolbarModule,
+    MatTooltipModule,
+    MatTreeModule,
+} from '@angular/material';
 
 @NgModule({
     declarations: [
@@ -14,7 +60,51 @@ import {HttpClientModule} from '@angular/common/http';
         BrowserModule,
         AppRoutingModule,
         HttpClientModule,
-        FormsModule
+        FormsModule,
+        BrowserAnimationsModule,
+        A11yModule,
+        CdkStepperModule,
+        CdkTableModule,
+        CdkTreeModule,
+        DragDropModule,
+        MatAutocompleteModule,
+        MatBadgeModule,
+        MatBottomSheetModule,
+        MatButtonModule,
+        MatButtonToggleModule,
+        MatCardModule,
+        MatCheckboxModule,
+        MatChipsModule,
+        MatStepperModule,
+        MatDatepickerModule,
+        MatDialogModule,
+        MatDividerModule,
+        MatExpansionModule,
+        MatGridListModule,
+        MatIconModule,
+        MatInputModule,
+        MatListModule,
+        MatMenuModule,
+        MatNativeDateModule,
+        MatPaginatorModule,
+        MatProgressBarModule,
+        MatProgressSpinnerModule,
+        MatRadioModule,
+        MatRippleModule,
+        MatSelectModule,
+        MatSidenavModule,
+        MatSliderModule,
+        MatSlideToggleModule,
+        MatSnackBarModule,
+        MatSortModule,
+        MatTableModule,
+        MatTabsModule,
+        MatToolbarModule,
+        MatTooltipModule,
+        MatTreeModule,
+        PortalModule,
+        ScrollingModule,
+        ReactiveFormsModule,
     ],
     providers: [],
     bootstrap: [AppComponent]
