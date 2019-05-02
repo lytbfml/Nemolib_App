e55806ede08cabd7165958d91e0ee2b026129408
Yangxiao Wang <lytbfml@gmail.com>
2019-05-02 01:01:02 -0700

    Revert "Revert "update new uwb nemolib api""
    This reverts commit 188f80666c824225715193cb20797186f8b2f48d.

--- nemolib_backend/src/main/java/edu/uwb/nemolib/SubgraphEnumerationResult.java
+++ nemolib_backend/src/main/java/edu/uwb/nemolib/SubgraphEnumerationResult.java
@@ -6,26 +6,26 @@ import java.util.Map;
  * Objects implementing SubgraphEnumerationResult are used by
  * SubgraphEnumerators to store the results of enumeration. For instance, a
  * SubgraphCount enumeration object tracks the frequency of each g6 label type
- *
  */
-public interface SubgraphEnumerationResult
-{
+public interface SubgraphEnumerationResult {
 	/**
 	 * Adds a Subgraph to this SubgraphEnumerationResult
+	 *
 	 * @param currentSubgraph a Subgraph to addSubgraph to this
-	 * SubgraphEnumerationResult
+	 *                        SubgraphEnumerationResult
 	 */
 	void addSubgraph(Subgraph currentSubgraph);
-
+	
 	/**
 	 * Merges g6 labels into SubgraphProfile labels. Recommended to use the
 	 * Labeler class to accomplish this.
 	 */
 	void label();
-
+	
 	/**
 	 * Calculates and returns a map of relative frequencies or concentrations
 	 * of labels in this subgraph.
+	 *
 	 * @return The map of relative frequencies
 	 */
 	Map<String, Double> getRelativeFrequencies();
--- nemolib_backend/src/main/java/edu/uwb/nemolib/SubgraphEnumerator.java
+++ nemolib_backend/src/main/java/edu/uwb/nemolib/SubgraphEnumerator.java
@@ -1,6 +1,5 @@
 package edu.uwb.nemolib;
 
 public interface SubgraphEnumerator {
-	public void enumerate(Graph targetGraph, int subgraphSize,
-	                      SubgraphEnumerationResult subgraphs);
+	public void enumerate(Graph targetGraph, int subgraphSize, SubgraphEnumerationResult subgraphs);
 }
